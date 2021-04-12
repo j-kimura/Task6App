16c82d49673ef58a0f262620cba25a5d657356e1
akio0911 <akio0911@gmail.com>
2021-04-12 22:59:48 +0900

    feedback

--- Task6App/ViewController.swift
+++ Task6App/ViewController.swift
@@ -10,37 +10,40 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet private weak var questionLabel: UILabel!
     @IBOutlet private weak var answerSlider: UISlider!
-    private var currentValue: String = "50"
+
+    private var answerValue = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        restartGame()
-        self.currentValue = "\(Int(answerSlider.value))"
-    }
-
-    @IBAction private func changeSlider(_ sender: Any) {
-        self.currentValue = "\(Int(self.answerSlider.value))"
+        resetGame()
     }
 
     @IBAction private func checkAnswerButton(_ sender: Any) {
-        guard questionLabel.text == self.currentValue else {
-            presentAlert(message: "はずれ!\nあなたの値：\(self.currentValue)")
-            return
+        let sliderValue = Int(answerSlider.value)
+
+        let firstLine: String
+
+        if answerValue == sliderValue {
+            firstLine = "あたり!"
+        } else {
+            firstLine = "はずれ!"
         }
-        presentAlert(message: "あたり!\nあなたの値：\(self.currentValue)")
+
+        presentAlert(message: "\(firstLine)\nあなたの値：\(sliderValue)")
     }
 
-    private func restartGame() {
-        self.questionLabel.text = "\(Int.random(in: 0...100))"
+    private func resetGame() {
+        answerValue = Int.random(in: 1...100)
+        questionLabel.text = String(answerValue)
         answerSlider.value = 50
-        }
+    }
 
     private func presentAlert(message: String) {
         let alert = UIAlertController(title: "結果", message: message, preferredStyle: .alert)
 
         let nextQuestionAction = UIAlertAction(
-            title: "再挑戦", style: .default, handler: { (_ : UIAlertAction!) -> Void in self.restartGame()})
+            title: "再挑戦", style: .default, handler: { [weak self] _ in self?.resetGame()})
         alert.addAction(nextQuestionAction)
 
         present(alert, animated: true, completion: nil)
